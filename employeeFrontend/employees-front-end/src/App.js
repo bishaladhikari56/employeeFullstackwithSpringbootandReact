17dd68b34416b00b505c20ccb9f3152ef2985759
bishaladhikari56 <70300391+bishaladhikari56@users.noreply.github.com>
2022-02-15 11:57:14 -0800

    employee front end updated

---
+++ employeeFrontend/employees-front-end/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import ListEmployeesComponent from "./components/ListEmployeesComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CreateEmployeeComponent from "./components/CreateEmployeeComponent";
+import ViewEmployeeComponent from "./components/ViewEmployeeComponent";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className='container'>
+          <Switch>
+            <Route path='/' exact component={ListEmployeesComponent}></Route>
+            <Route path='/employees' component={ListEmployeesComponent}></Route>
+            <Route
+              path='/add-employee/:id'
+              component={CreateEmployeeComponent}
+            ></Route>
+            <Route
+              path='/view-employee/:id'
+              component={ViewEmployeeComponent}
+            ></Route>
+            {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+          </Switch>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
